c77767f06aa134f74395e2caae3e8958d1aece6c
david.jahn <david.jahn@192.168.0.143>
2018-11-13 00:34:49 +0100

    Added feature to view stack trace

--- 04QuizCandidateV1/src/pkgMisc/ExceptionHandler.java
+++ 04QuizCandidateV1/src/pkgMisc/ExceptionHandler.java
@@ -1,7 +1,14 @@
 package pkgMisc;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 public class ExceptionHandler implements IStaticStrings
 {
@@ -24,9 +31,33 @@ public class ExceptionHandler implements IStaticStrings
 	alert.setTitle("Error");
 	alert.setHeaderText("And unexpected error occured");
 	alert.setContentText(ex.getClass() + ": " + ex.getMessage());
-	alert.showAndWait();
+	// Create expandable Exception.
+	StringWriter sw = new StringWriter();
+	PrintWriter pw = new PrintWriter(sw);
+	ex.printStackTrace(pw);
+	String exceptionText = sw.toString();
 
+	Label label = new Label("The exception stacktrace was:");
+
+	TextArea textArea = new TextArea(exceptionText);
+	textArea.setEditable(false);
+	textArea.setWrapText(true);
+
+	textArea.setMaxWidth(Double.MAX_VALUE);
+	textArea.setMaxHeight(Double.MAX_VALUE);
+	GridPane.setVgrow(textArea, Priority.ALWAYS);
+	GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+	GridPane expContent = new GridPane();
+	expContent.setMaxWidth(Double.MAX_VALUE);
+	expContent.add(label, 0, 0);
+	expContent.add(textArea, 0, 1);
+
+	// Set expandable Exception into the dialog pane.
+	alert.getDialogPane().setExpandableContent(expContent);
 	Logger.doWriteLogFile(ex);
+	alert.showAndWait();
+	
 	if (debug)
 	    ex.printStackTrace();
     }
@@ -37,11 +68,36 @@ public class ExceptionHandler implements IStaticStrings
 	alert.setTitle("Error");
 	alert.setHeaderText(title);
 	alert.setContentText(ex.getMessage());
-	alert.showAndWait();
+	// Create expandable Exception.
+	StringWriter sw = new StringWriter();
+	PrintWriter pw = new PrintWriter(sw);
+	ex.printStackTrace(pw);
+	String exceptionText = sw.toString();
+
+	Label label = new Label("The exception stacktrace was:");
+
+	TextArea textArea = new TextArea(exceptionText);
+	textArea.setEditable(false);
+	textArea.setWrapText(true);
+
+	textArea.setMaxWidth(Double.MAX_VALUE);
+	textArea.setMaxHeight(Double.MAX_VALUE);
+	GridPane.setVgrow(textArea, Priority.ALWAYS);
+	GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+	GridPane expContent = new GridPane();
+	expContent.setMaxWidth(Double.MAX_VALUE);
+	expContent.add(label, 0, 0);
+	expContent.add(textArea, 0, 1);
+
+	// Set expandable Exception into the dialog pane.
+	alert.getDialogPane().setExpandableContent(expContent);
 
 	Logger.doWriteLogFile(ex);
+	alert.showAndWait();
 	if (debug)
 	    ex.printStackTrace();
+	
     }
 
 }
