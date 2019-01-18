22101dfddd742e4cac8491c0adfec71d8f3a0827
David <david.jahn2000@gmail.com>
2019-01-18 09:40:22 +0100

    initial commit

--- 09FX_Animations_V2/src/pkgMain/Controller_GUI_Main.java
+++ 09FX_Animations_V2/src/pkgMain/Controller_GUI_Main.java
@@ -1,5 +1,34 @@
 package pkgMain;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class Controller_GUI_Main {
-	
+
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private Button btnShowResult;
+
+    @FXML
+    private Button btnStop;
+
+    @FXML
+    private Label lblMessage;
+
+    @FXML
+    private Label lblMessage2;
+
+    @FXML
+    private ImageView imgCar1;
+
+    @FXML
+    void onSelectButton(ActionEvent event) {
+
+    }
+
 }
--- 09FX_Animations_V2/src/pkgMain/Main.java
+++ 09FX_Animations_V2/src/pkgMain/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("GUI_Main.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("ressources/GUI_Main.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
